fa1928cff1e155a91bef92b9ecf4185a1ccb62e9
roman.vasilev <>
2018-08-24 22:01:35 +0400

    Fix: Removed tslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,3 @@
-const tslintConfigRulesDirectory = [
-    'node_modules/tslint/lib/rules',
-    // 'node_modules/tslint-clean-code/dist/src',
-];
-const tslintConfigRules = Object.assign({},
-    require('tslint/lib/configs/recommended').rules,
-    // require('tslint-clean-code/recommended_ruleset').rules,
-    {
-        'member-access': false,
-        'ordered-imports': false,
-        'quotemark': false,
-        'no-var-keyword': false,
-        'object-literal-sort-keys': false,
-        'no-console': false,
-        'arrow-parens': false,
-        'max-line-length': false,
-        'object-literal-key-quotes': false,
-        'no-shadowed-variable': false,
-        'only-arrow-functions': false,
-        'no-var-requires': false,
-        'semicolon': false,
-        'interface-over-type-literal': false,
-        'align': false,
-        'trailing-comma': false,
-        'typedef': false,
-        'newline-before-return': false,
-        'no-require-imports': false,
-    });
-
 module.exports = {
     'root': true,
     'env': {
@@ -46,17 +17,12 @@ module.exports = {
         'unicorn',
         'typescript',
         'import',
-        'tslint',
     ],
     'rules': {
         'no-undef': 0,
         'no-unused-vars': 0,
         'indent': 0,
         'unicorn/import-index': 0,
-        'tslint/config': [1, {
-            rules: tslintConfigRules,
-            rulesDirectory: tslintConfigRulesDirectory,
-        }],
         'import/newline-after-import': 0,
         'import/no-duplicates': 1,
         'import/max-dependencies': [1, { 'max': 10 }],
